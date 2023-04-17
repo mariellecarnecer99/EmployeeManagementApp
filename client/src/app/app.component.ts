8c9d3c27fcb0b3f46be1fda1011d066860d0330c
maynion <mcarnecer@startechup.com>
2023-04-17 18:25:16 +0800

    integrate register

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from './_services/account.service';
 import { User } from './_models/user';
@@ -12,19 +11,10 @@ export class AppComponent implements OnInit {
   title = 'Employee Management App';
   users: any;
 
-  constructor(private http: HttpClient, private accountService: AccountService) {}
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:3001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed') 
-    });
   }
 
   setCurrentUser() {
