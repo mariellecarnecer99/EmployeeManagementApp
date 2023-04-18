30329e3a13491aaa1f94100dc3cfe5ac2e6c46ba
maynion <mcarnecer@startechup.com>
2023-04-18 12:20:43 +0800

    integrate other components and setup routing

---
+++ client/src/app/messages/messages.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messagess',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent {
+
+}
