70fcb12da8284ed070e03394dad2513a283922cc
maynion <mcarnecer@startechup.com>
2023-04-20 11:19:16 +0800

    Updating resources

---
+++ API/DTOs/MemberUpdateDto.cs
@@ -0,0 +1,10 @@
+namespace API.DTOs
+{
+    public class MemberUpdateDto
+    {
+        public string Age { get; set; }
+        public string Position { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
